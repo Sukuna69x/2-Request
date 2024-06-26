9d54f9bdf5c47def0120f424bd1671a22e64dd18
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-06-26 21:19:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,23 +1,47 @@
-#(©)CodeXBotz
-
-
-
+# Credit- @StupidBoi69
 
 import os
 import asyncio
+#token
+import base64
+import logging
+import random
+import re
+import string
+import time
+#token
+
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, CHANNEL_ONE, CHANNEL_TWO
-from helper_func import encode, decode, get_messages
+from config import (
+    ADMINS,
+    FORCE_MSG,
+    START_MSG,
+    CUSTOM_CAPTION,
+    PROTECT_CONTENT,
+    DISABLE_CHANNEL_BUTTON,
+    CHANNEL_ONE,
+    CHANNEL_TWO,
+    IS_VERIFY, #token
+    VERIFY_EXPIRE,
+    SHORTLINK_API,
+    SHORTLINK_URL,
+    TUT_VID,
+    OWNER_ID, #token
+)
+                                                                    #token
+from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
 from database.database import add_user, del_user, full_userbase, present_user, is_requested_one, is_requested_two, delete_all_one, delete_all_two
+#token
+from database.db_token import * 
+from shortzy import Shortzy
+#token
 
-
-
-
+'''
 @Bot.on_message(filters.command('start') & filters.private)
 async def start_command(client: Client, message):
     id = message.from_user.id
@@ -173,15 +197,256 @@ async def start_command(client: Client, message):
             quote = True
         )
         return
+'''
 
-    
-#=====================================================================================##
 
-WAIT_MSG = """"<b>Processing ...</b>"""
+@Bot.on_message(filters.command('start') & filters.private & subscribed)
+async def start_command(client: Client, message: Message):
+    id = message.from_user.id
+    owner_id = ADMINS  # Fetch the owner's ID from config
 
-REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
+    # Check if the user is the owner
+    if id == owner_id:
+        # Owner-specific actions
+        # You can add any additional actions specific to the owner here
+        await message.reply("You are the owner! Additional actions can be added here.")
 
-#=====================================================================================##
+    else:
+        if not await present_user(id):
+            try:
+                await add_user(id)
+            except:
+                pass
+
+        verify_status = await get_verify_status(id)
+        if verify_status['is_verified'] and VERIFY_EXPIRE < (time.time() - verify_status['verified_time']):
+            await update_verify_status(id, is_verified=False)
+
+        if "verify_" in message.text:
+            _, token = message.text.split("_", 1)
+            if verify_status['verify_token'] != token:
+                return await message.reply("Your token is invalid or Expired. Try again by clicking /start")
+            await update_verify_status(id, is_verified=True, verified_time=time.time())
+            if verify_status["link"] == "":
+                reply_markup = None
+            await message.reply(f"Your token successfully verified and valid for: 24 Hour", reply_markup=reply_markup, protect_content=False, quote=True)
+
+        elif len(message.text) > 7 and verify_status['is_verified']:
+            try:
+                base64_string = message.text.split(" ", 1)[1]
+            except:
+                return
+            _string = await decode(base64_string)
+            argument = _string.split("-")
+            if len(argument) == 3:
+                try:
+                    start = int(int(argument[1]) / abs(client.db_channel.id))
+                    end = int(int(argument[2]) / abs(client.db_channel.id))
+                except:
+                    return
+                if start <= end:
+                    ids = range(start, end+1)
+                else:
+                    ids = []
+                    i = start
+                    while True:
+                        ids.append(i)
+                        i -= 1
+                        if i < end:
+                            break
+            elif len(argument) == 2:
+                try:
+                    ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+                except:
+                    return
+            temp_msg = await message.reply("Please wait...")
+            try:
+                messages = await get_messages(client, ids)
+            except:
+                await message.reply_text("Something went wrong..!")
+                return
+            await temp_msg.delete()
+            
+            snt_msgs = []
+            
+            for msg in messages:
+                if bool(CUSTOM_CAPTION) & bool(msg.document):
+                    caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, filename=msg.document.file_name)
+                else:
+                    caption = "" if not msg.caption else msg.caption.html
+
+                if DISABLE_CHANNEL_BUTTON:
+                    reply_markup = msg.reply_markup
+                else:
+                    reply_markup = None
+
+                try:
+                    snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+                    await asyncio.sleep(0.5)
+                    snt_msgs.append(snt_msg)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+                    snt_msgs.append(snt_msg)
+                except:
+                    pass
+
+            SD = await message.reply_text("Baka! Files will be deleted After 300 seconds. Save them to the Saved Message now!")
+            await asyncio.sleep(300)
+
+            for snt_msg in snt_msgs:
+                try:
+                    await snt_msg.delete()
+                    await SD.delete()
+                except:
+                    pass
+
+        elif verify_status['is_verified']:
+            reply_markup = InlineKeyboardMarkup(
+                [[InlineKeyboardButton("About Me", callback_data="about"),
+                  InlineKeyboardButton("Close", callback_data="close")]]
+            )
+            await message.reply_text(
+                text=START_MSG.format(
+                    first=message.from_user.first_name,
+                    last=message.from_user.last_name,
+                    username=None if not message.from_user.username else '@' + message.from_user.username,
+                    mention=message.from_user.mention,
+                    id=message.from_user.id
+                ),
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+                quote=True
+            )
+
+        else:
+            verify_status = await get_verify_status(id)
+            if IS_VERIFY and not verify_status['is_verified']:
+                short_url = f"instantearn.in"
+                # TUT_VID = f"https://t.me/ultroid_official/18"
+                token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+                await update_verify_status(id, verify_token=token, link="")
+                link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
+                btn = [
+                    [InlineKeyboardButton("Click here", url=link)],
+                    [InlineKeyboardButton('How to use the bot', url=TUT_VID)]
+                ]
+                await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 Hour after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
+
+
+'''
+@Bot.on_message(filters.command('start') & filters.private)
+async def not_joined(client: Client, message: Message):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                "Join Channel",
+                url = client.invitelink)
+        ]
+    ]
+    try:
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text = 'Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+            ]
+        )
+    except IndexError:
+        pass
+
+    await message.reply(
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
+        )
+'''
+
+
+@Bot.on_message(filters.command('start') & filters.private)
+async def not_joined(lient: Client, message):
+    id = message.from_user.id
+    if not await present_user(id):
+        try:
+            await add_user(id)
+        except:
+            pass
+    text = message.text
+    if len(text)>7:
+        if client.link_one is not None and message.from_user.id not in ADMINS and not await is_requested_one(message):
+            btn = [[
+                InlineKeyboardButton(
+                    "Request To Join Channel 1", url=client.link_one)
+            ]]
+            try:
+                if client.link_two is not None and message.from_user.id not in ADMINS and not await is_requested_two(message):
+                    btn.append(
+                          [
+                        InlineKeyboardButton(
+                            "Request To Join Channel 2", url=client.link_two)
+                          ]
+                    )
+            except Exception as e:
+                print(e)
+            try:
+                btn.append(
+                      [
+                        InlineKeyboardButton(
+                             text = 'Try Again',
+                             url = f"https://t.me/{client.username}?start={message.command[1]}"
+                        )
+                    ]
+                    )
+            except (IndexError, ValueError):
+                pass
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text="**Please request Join the Following Channels to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(inline_keyboard=btn),
+                parse_mode=ParseMode.MARKDOWN
+            )
+            return
+        if client.link_two is not None and message.from_user.id not in ADMINS and not await is_requested_two(message):
+            btn = [[
+                InlineKeyboardButton(
+                    "Request To Join Channel 2", url=client.link_two)
+            ]]
+            try:
+                if client.link_one is not None and message.from_user.id not in ADMINS and not await is_requested_one(message):
+                    btn.append(
+                          [
+                        InlineKeyboardButton(
+                            "Request To Join Channel 1", url=client.link_one)
+                          ]
+                    )
+            except Exception as e:
+                print(e)
+            try:
+                btn.append(
+                      [
+                        InlineKeyboardButton(
+                             text = 'Try Again',
+                             url = f"https://t.me/{client.username}?start={message.command[1]}"
+                        )
+                    ]
+                    )
+            except (IndexError, ValueError):
+                pass
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text="**Please request Join the Following Channels to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(inline_keyboard=btn),
+                parse_mode=ParseMode.MARKDOWN
+            )
+            return
 
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
@@ -213,7 +478,7 @@ async def send_text(client: Bot, message: Message):
             except UserIsBlocked:
                 await del_user(chat_id)
                 blocked += 1
-            except InputUserDeactivated:
+           except InputUserDeactivated:
                 await del_user(chat_id)
                 deleted += 1
             except:
@@ -249,3 +514,7 @@ async def purge_req_two(bot, message):
     r = await message.reply("`processing...`")
     await delete_all_two()
     await r.edit("**Req db Cleared**" )
+
+    
+WAIT_MSG = """"<b>Processing ...</b>"""
+REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
