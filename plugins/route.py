e48378794993e170331c8a0c9e0147312976e006
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-16 09:39:51 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,8 +1,9 @@
-#rymme
+#Credit- Telegram @StupidBoi69
+
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("VysakhTG")
+    return web.json_response("StupidBoi")
