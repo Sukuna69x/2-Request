e933f9043c698dae846619d38be1845750e87a81
Sukuna69x <niloygain29@gmail.com>
2024-09-18 02:33:15 +0530

    Update req_svd.py

--- plugins/req_svd.py
+++ plugins/req_svd.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest
 from bot import Bot
-from config import CHANNEL_ONE, CHANNEL_TWO, CHANNEL_THREE
-from database.database import add_req_one, add_req_two, add_req_three
+from config import CHANNEL_ONE, CHANNEL_TWO
+from database.database import add_req_one, add_req_two
 
 @Bot.on_chat_join_request(
-    filters.chat(CHANNEL_ONE) | filters.chat(CHANNEL_TWO) | filters.chat(CHANNEL_THREE)
+    filters.chat(CHANNEL_ONE) | filters.chat(CHANNEL_TWO)
 )
 async def join_reqs(client, join_req: ChatJoinRequest):
     user_id = join_req.from_user.id
@@ -19,10 +19,3 @@ async def join_reqs(client, join_req: ChatJoinRequest):
             await add_req_two(user_id)
         except Exception as e:
             print(f"Error adding join request to req_two: {e}")
-
-    elif join_req.chat.id == CHANNEL_THREE:
-        try:
-            await add_req_three(user_id)
-        except Exception as e:
-            print(f"Error adding join request to req_three: {e}")
-            
